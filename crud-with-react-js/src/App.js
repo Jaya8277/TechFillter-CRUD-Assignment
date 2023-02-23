b04e360d224d75516ed4025bbe764ded5c070741
Jaya Vishwakarma <jayavish423@gmail.com>
2023-02-24 03:11:59 +0530

    Crud app with react completed

--- crud-with-react-js/src/App.js
+++ crud-with-react-js/src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
-
+import {Routes,Route} from 'react-router-dom';
+import AddEmployee from './Components/AddEmployee';
+import Employee from './Components/Employee';
+import Navbar from './Components/Navbar';
+import EditEmploye from './Components/EditEmploye';
+import Details from './Components/Details';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Navbar/>
+    <Routes>
+      <Route path='/' element={<Employee/>}/> 
+      <Route path='/add' element={<AddEmployee/>}/>
+      <Route path='/edit/:id' element={<EditEmploye/>}/>
+      <Route path='/detail/:id' element={<Details/>}/>
+    </Routes>
     </div>
   );
 }
---
+++ crud-with-react-js/src/Components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import './Navbar.css';
+const Navbar = () => {
+  return (
+    <div className='main'>
+        <Link to="/" className='main1'><h1>Home</h1></Link>
+        <Link to="/add" className='main1'><h1>Add</h1></Link>
+    </div>
+  )
+}
+
+export default Navbar
